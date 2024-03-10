acbd248d1516ac3cac1a12ce46ff9c3639ce9ec6
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-03-09 20:09:38 -0600

    Análisis de Resultados

--- Guia_3/FrmUsuario.xaml.cs
+++ Guia_3/FrmUsuario.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Guia_3.DAOs;
 using Guia_3.DTOs;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Guia_3
 {
@@ -43,24 +46,93 @@ namespace Guia_3
 
         private void btnRegistrar_Click(object sender, RoutedEventArgs e)
         {
-            UsuarioDao daoUsuario = new();
-
             try
             {
-                PaisDto paisSeleccionado = (PaisDto)txtPais.SelectedItem;
-                int paisId = paisSeleccionado?.PaisId ?? 0;
+                if (ValidarCampos())
+                {
+                    UsuarioDao daoUsuario = new();
+                    PaisDto paisSeleccionado = (PaisDto)txtPais.SelectedItem;
+                    int paisId = paisSeleccionado?.PaisId ?? 0;
 
-                if (this.Id == null)
-                    daoUsuario.Agregar(txtNombre.Text, txtApellido.Text, txtEmail.Text, paisId);
+                    if (this.Id == null)
+                        daoUsuario.Agregar(txtNombre.Text, txtApellido.Text, txtEmail.Text, paisId);
+                    else
+                        daoUsuario.Actualizar(txtNombre.Text, txtApellido.Text, txtEmail.Text, paisId, Id.Value);
+
+                    this.Close();
+                }
                 else
-                    daoUsuario.Actualizar(txtNombre.Text, txtApellido.Text, txtEmail.Text, paisId, Id.Value);
-
-                this.Close();
+                {
+                    MessageBox.Show("Por favor, corrige los errores en los campos.", "Error de validación", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
             catch (Exception ex)
             {
-                throw new Exception("Error:" + ex.Message);
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+
+        private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidarTexto(txtNombre);
+        }
+
+        private void txtApellido_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidarTexto(txtApellido);
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidarEmail(txtEmail.Text);
+        }
+
+        private bool ValidarTexto(TextBox txt)
+        {
+            string regex = "[a-zA-Z]+$"; // Solo letras de la a-z o A-Z
+
+            if (!Regex.IsMatch(txt.Text, regex))
+            {
+                txt.BorderBrush = Brushes.Red;
+                return false;
+            }
+            else
+            {
+                txt.BorderBrush = Brushes.Black;
+                return true;
+            }
+        }
+
+        private bool ValidarEmail(string email)
+        {
+            string regex = @"^([\w\.\-]+)@([\w\-]+)\.([a-zA-Z]{2,6})$";
+            if (!Regex.IsMatch(email, regex))
+            {
+                txtEmail.BorderBrush = Brushes.Red;
+                return false;
+            }
+            else
+            {
+                txtEmail.BorderBrush = Brushes.Black;
+                return true;
+            }
+        }
+
+        private bool ValidarCampos()
+        {
+            bool esValido = true;
+
+            if (!ValidarTexto(txtNombre))
+                esValido = false;
+
+            if (!ValidarTexto(txtApellido))
+                esValido = false;
+
+            if (!ValidarEmail(txtEmail.Text))
+                esValido = false;
+
+            return esValido;
         }
     }
 }
